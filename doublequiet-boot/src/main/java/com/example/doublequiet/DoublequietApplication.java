b121587665a382e349244506aa0a531f981562d6
tanjing <526211447@qq.com>
2020-03-26 02:13:40 +0800

    后台代码

---
+++ doublequiet-boot/src/main/java/com/example/doublequiet/DoublequietApplication.java
@@ -0,0 +1,13 @@
+package com.example.doublequiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublequietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoublequietApplication.class, args);
+    }
+
+}
---
+++ doublequiet-boot/src/test/java/com/example/doublequiet/DoublequietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doublequiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoublequietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
